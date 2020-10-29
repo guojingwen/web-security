66a51106285a4ad436f2af7b59f81323b322d5de
guojw <2315162186@qq.com>
2020-10-30 00:01:08 +0800

    生产环境配置

--- web-security-server/models/connection.js
+++ web-security-server/models/connection.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 exports.getConnection = function(){
 	let connection = mysql.createConnection({
-		host: 'localhost',
-		database: 'safety',
+		host: (process.env.NODE_ENV === 'production') ? 'localhost:3308': 'localhost',
+		database: (process.env.NODE_ENV === 'production') ? 'safety_new': 'safety',
 		user: 'root',
 		password: 'todoDream'
 	});
